34c6432c49aebdf7e5df390bf1f584d44fdf2aa0
Shyan W. <shyanw321@gmail.com>
2024-05-06 19:03:58 -0400

    Changed DB URL

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
  mongoose.set('strictQuery', false);
 // MongoDB connection URI from environment variable
-const connectionString = process.env.CONNECTION_URI 
+const connectionString = process.env.CONNECTION_URI || 'mongodb+srv://shyan:test@cluster0.vou8vwx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 // Connect to MongoDB
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(process.env.CONNECTION_URI ))
