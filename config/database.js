e105b30c95381bf2eafec753492acb8ab77cf6dc
Shyan W. <shyanw321@gmail.com>
2024-05-06 15:04:31 -0400

    Changed DB URL

--- config/database.js
+++ config/database.js
@@ -7,9 +7,8 @@ dotenv.config()
 mongoose.set('strictQuery', false);
 
 // Use the environment variable from Netlify
-const DATABASE_URL = process.env.VITE_BACK_END_SERVER_URL;
 
-mongoose.connect(DATABASE_URL);
+mongoose.connect(process.env.DATABASE.URL);
 
 db.on('connected', function () {
   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
